d8c4aa370554b412a2e157d8c9bbdb6460f84ef8
Jeremy <1078293700@qq.com>
2016-03-24 16:03:35 +0800

    Update config.php

--- APF/Config/config.php
+++ APF/Config/config.php
@@ -1,7 +1,7 @@
 return [
-	//配置项 => 配置值,
-	'DEFAULT_TIME_ZONE' => 'PRC',
-    'SESSION_AUTO_START' => FALSE,
+     //配置项 => 配置值,
+    'DEFAULT_TIME_ZONE' => 'PRC',
+    'SESSION_START' => FALSE,
     'VAR_CONTROLLER' => 'c',
     'VAR_ACTION' => 'a',
 ];
